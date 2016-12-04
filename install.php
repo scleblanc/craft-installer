c4aa19e4d102e2d7fb3b464f8eed25c593d228b9
Sam LeBlanc <sam@newmediacampaigns.com>
2016-12-04 15:55:22 -0500

    updates to install.php

--- install.php
+++ install.php
@@ -7,7 +7,7 @@ $inputs =
 	'email' => $_REQUEST['email'],
 	'password' => $_REQUEST['password'],
 	'siteName' => $_REQUEST['site_name'],
-	'siteUrl' => $_REQUEST['project_name'].".nmc",
+	'siteUrl' => $_REQUEST['project_name'].".dev", // or .whatever
 ];
 $craft->init();
 $craft->install->run($inputs);
